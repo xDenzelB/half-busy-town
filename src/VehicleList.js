b7293b5bf45a9fd07dbf07ba901ea0dfd16311ff
Denzel <dbartolaba123@gmail.com>
2022-01-26 14:42:28 -0800

    Added in vehicle and vehicleList js

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Vehicle from './Vehicle';
 
-export default function VehicleList() {
+export default function VehicleList(props) {
   return (
     <div className="traffic">
+      {props.carsArray.map((vehicle, i) => 
+        <Vehicle key ={vehicle + i} vehicle=
+          {vehicle} />
+      )}
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
       {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
